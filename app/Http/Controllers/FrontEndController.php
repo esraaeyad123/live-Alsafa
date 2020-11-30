4139a0917b3209449ce77dc084d4af38423fbcab
esraaeyad123 <47371491+esraaeyad123@users.noreply.github.com>
2020-11-30 20:25:05 +0300

    Update FrontEndController.php

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Input;
+
 use Session;
 use Validator;
-
+use App\subcategories;
 use App\Contact;
 use App\Category;
+use DB;
+
+
+
 use Illuminate\Http\Request;
 
 class FrontEndController extends Controller
@@ -14,38 +20,55 @@ class FrontEndController extends Controller
 
 
 
-    public function index(Category $categories)
-    {
+    public function index(Category $category) {
+
+       // $subcategories = $category->subcategories();
+
+
         $categories = Category::all();
-    //  return view('offer');
+       $subcategories = DB::table('subcategories')->select('*')->where('parent_id', '!=', NULL)->get();
 
-        return view('offer', compact('categories'));
+        return view('offer', compact(['categories', 'subcategories']));
+ }
 
 
+
+    public function subCat(Request $request)
+    {
+
+        $parent_id = $request->cat_id;
+
+
+        $subcategories = subcategories::where('parent_id', '=', $parent_id)->get();
+
+        //  dd( $subcategories);
+        return response()->json([
+            'subcategories' => $subcategories
+        ]);
     }
     public function send_message(Request $request){
 
 
 
         $this->validate($request, [
-            'name' => 'required',
-            'phone' => 'required',
-            'subject' => 'required',
+
+            'subcategories_id' => 'required',
             'message' => 'required',
             'category_id' => 'required',
         ]);
 
         $contact = Contact::create([
-            'name' => $request->name,
-            'subject' => $request->subject,
+
+            'subcategories_id' => $request->subcategories_id,
             'message' => $request->message,
-            'phone' => $request->phone,
+
             'category_id' => $request->category_id,
         ]);
         $contact->save();
 if($contact){
-    Session::flash('send_message', 'تم ارسال الرسالة بنجاح');
+    Session::flash('send_message', 'تم ارسال الرسالة بنجاح شكرا لتواصلكم');
         return redirect()->back();}
     }
 
 }
+
