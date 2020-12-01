be24fcb319a24b41621070f6fa8c33fba7b31bdf
esraaeyad123 <47371491+esraaeyad123@users.noreply.github.com>
2020-12-01 17:53:18 +0300

    Add files via upload

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
@@ -10,6 +11,8 @@
   <link rel="stylesheet" href="{{ asset('admin') }}/css/adminlte.min.css">
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
   @yield('style')
 </head>
 <body class="hold-transition sidebar-mini">
@@ -44,30 +47,39 @@
     <!-- Sidebar -->
     <div class="sidebar">
       <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="{{ asset('admin') }}/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="image">
+                <img src="{{ asset('admin') }}/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+            </div>
+            <div class="info">
+                <a href="{{ route('user.profile') }}" class="d-block">{{ Auth::user()->name }}</a>
+            </div>
         </div>
-        <div class="info">
-          <a href="/" class="d-block">{{ Auth::user()->name }}</a>
-        </div>
-      </div>
 
-      <!-- Sidebar Menu -->
+
+        <!-- Sidebar Menu -->
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <li class="nav-item mt-auto">
-            <a href="{{ route('category.index') }}" class="nav-link {{ (request()->is('admin/category*')) ? 'active': '' }}">
-              <i class="nav-icon fas fa-tags"></i>
-              <p>
-                Categories
-              </p>
-            </a>                    
-          </li>
+            <li class="nav-item mt-auto">
+                <a href="{{ route('category.index') }}" class="nav-link {{ (request()->is('admin/category*')) ? 'active': '' }}">
+                    <i class="nav-icon fas fa-tags"></i>
+                    <p>
+                        Categories
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item mt-auto">
+                <a href="{{ route('subcategories.index') }}" class="nav-link {{ (request()->is('admin/subcategories*')) ? 'active': '' }}">
+                    <i class="nav-icon fas fa-tags"></i>
+                    <p>
+                        subcategories
+                    </p>
+                </a>
+            </li>
 
-          <li class="nav-item mt-auto">
+            <li class="nav-item mt-auto">
             <a href="{{ route('contact.index') }}" class="nav-link {{ (request()->is('admin/contact*')) ? 'active': '' }}">
               <i class="nav-icon fas fa-envelope"></i>
               <p>
@@ -84,21 +96,38 @@
             </a>
           </li>
 
-          <li class="nav-item mt-auto">
-            <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="nav-link">
-              <i class="nav-icon fas fa-sign-out-alt"></i>
-              <p>
-                Logout
-              </p>
-            </a>
-          </li>
-          <li class="text-center mt-5">
-            <a href="/" class="btn btn-primary text-white" target="_blank">
-              <p class="mb-0">
-                View Website
-              </p>
-            </a>                    
-          </li>
+            <li class="nav-item mt-auto">
+                <a href="{{ route('user.index') }}" class="nav-link {{ (request()->is('admin/user*')) ? 'active': '' }}">
+                    <i class="nav-icon fas fa-user"></i>
+                    <p>
+                        User
+                    </p>
+                </a>
+            </li>
+            <li class="nav-header">Your Account</li>
+            <li class="nav-item mt-auto">
+                <a href="{{ route('user.profile') }}" class="nav-link {{ (request()->is('admin/profile')) ? 'active': '' }}">
+                    <i class="nav-icon far fa-user"></i>
+                    <p>
+                        Your Profile
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item mt-auto">
+                <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="nav-link">
+                    <i class="nav-icon fas fa-sign-out-alt"></i>
+                    <p>
+                        Logout
+                    </p>
+                </a>
+            </li>
+            <li class="text-center mt-5">
+                <a href="{{ route('website') }}" class="btn btn-primary text-white" target="_blank">
+                    <p class="mb-0">
+                        View Website
+                    </p>
+                </a>
+            </li>
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
