0007a93afd4b3a95882caaa9d2eeabc0cfd138c6
esraaeyad123 <47371491+esraaeyad123@users.noreply.github.com>
2020-12-15 21:16:03 +0300

    Update FrontEndController.php

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -28,12 +28,16 @@ class FrontEndController extends Controller
         $categories = Category::all();
        $subcategories = DB::table('subcategories')->select('*')->where('parent_id', '!=', NULL)->get();
      //   dd(Session(['key'=>'value']));
-if(Session(['key'=>'value'])){
-    
+if (!isset($_COOKIE['firsttime'])){
+        setcookie("firsttime", "no", /* EXPIRE */);
+
         return view('vip-rest', compact(['categories', 'subcategories']));
-}
+    
+        exit();
+
         else{
                  return view('inst', compact(['categories', 'subcategories']));
+    exit();
 
         }
 
